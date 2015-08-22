77e0e9976cfa5bf518e7ff0ba6653b06a611c403
Vison <kk801030@gmail.com>
2015-08-22 21:04:42 +0800

    distance done

--- public/js/meal-list.js
+++ public/js/meal-list.js
@@ -1,7 +1,10 @@
 angular.module('meal-list', [])
 
+
 .controller('listController', ['$scope', '$http', function($scope, $http) {
     $scope.meals;
+    $scope.distances;
+
     var req = {
      method: 'GET',
      url: 'https://api.parse.com/1/classes/Meal',
@@ -13,10 +16,30 @@ angular.module('meal-list', [])
     };
 
     $http(req).then(function(res){
-      $scope.meals = res.data.results;
+      meals = res.data.results;
+      console.log(meals);
+      for (var i = 0; i< meals.length; i++) {
+          meals[i].dis = distance(meals[i].location.latitude, meals[i].location.longitude, $scope.location.lat, $scope.location.lng);
+      };
+      $scope.meals = meals;
       console.log(res);
     }, function(){
 
     });
 
 }])
+
+function distance(lat1,lon1,lat2,lon2) {
+ var R = 6371; // km (change this constant to get miles)
+ var dLat = (lat2-lat1) * Math.PI / 180;
+ var dLon = (lon2-lon1) * Math.PI / 180;
+ var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+  Math.cos(lat1 * Math.PI / 180 ) * Math.cos(lat2 * Math.PI / 180 ) *
+  Math.sin(dLon/2) * Math.sin(dLon/2);
+ var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+ var d = R * c;
+ if (d>1) return Math.round(d)+"km";
+ else if (d<=1) return Math.round(d*1000)+"m";
+ return d;
+}
+
